37b590f37a7529c4813628aa44b0c8bd63e9a836
Batool_Dahnon <batooldahnon4@gmail.com>
2021-10-09 00:26:08 +0300

    version1.0

---
+++ app/src/main/java/com/example/file1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.file1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+   private EditText edtName;
+    private EditText edtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtName = findViewById(R.id.edtName);
+        edtPassword = findViewById(R.id.edtPassword);
+    }
+
+    public void btnSubmitOnClick(View view) {
+      String name =edtName.getText().toString();
+      String password= edtPassword.getText().toString();
+      String msg ="Name:" + name +"  "+ "password:" + password;
+        Toast.makeText(this,msg, Toast.LENGTH_LONG).show();
+    }
+}
